4574e5dc7f7b1ed93f644f7de55cf8a83eac5adf
parkerallan <parkerallan2000@gmail.com>
2025-01-25 19:06:30 -0600

    title render

--- code/main.c
+++ code/main.c
@@ -1,13 +1,32 @@
 #include <libdragon.h>
-#include <stdio.h>
+#include <stdlib.h>
+
+sprite_t *libdr_title;
+
+static char *title_sprites[] = {
+    "rom:/libdragon.sprite",
+    "rom:/tiny3d.sprite",
+    "rom:/startscreen.sprite",
+};
 
 int main(void) {
-    display_init(RESOLUTION_320x240, DEPTH_16_BPP, 2, GAMMA_NONE, ANTIALIAS_RESAMPLE);
-    console_init();
-    console_set_render_mode(RENDER_AUTOMATIC);
+    debug_init_isviewer();
+    debug_init_usblog();
 
-    console_clear();
-    printf("Hello, World!\n");
+	display_init(RESOLUTION_320x240, DEPTH_16_BPP, 2, GAMMA_NONE, FILTERS_RESAMPLE);
+	dfs_init(DFS_DEFAULT_LOCATION);
 
-    console_render();
+    joypad_init();
+    timer_init();
+
+    // rdpq_init();
+    // rdpq_debug_start();
+
+	libdr_title = sprite_load(title_sprites[0]);
+
+	while (1) {
+        surface_t* disp = display_get();
+		display_show(disp);
+		graphics_draw_sprite_trans(disp, 0, 0, libdr_title);
+    }
 }
